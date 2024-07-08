fbaafbdfd5681dafb6ee86d1d8def1e14fedfd1d
nicoam89 <nico.am89@gmail.com>
2024-07-08 13:28:42 -0300

    se corrige formulario de contacto en PHP

---
+++ contacto.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_POST['nombre']) && isset($_POST['email']) && isset($_POST['mensaje'])) {
+
+  // Validar datos (opcional)
+
+  $nombre = $_POST['nombre'];
+  $email = $_POST['email'];
+  $mensaje = $_POST['mensaje'];
+
+  // Enviar correo electrónico
+  $para = "ssydconsultora@gmail.com";
+  $asunto = "Contacto web - " . $email;
+  $cuerpo = "Nombre: $nombre \nCorreo electrónico: $email \nMensaje: $mensaje";
+  $cabecera = "MIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nFrom: $nombre <$email>\n";
+
+  if (mail($para, $asunto, $cuerpo, $cabecera)) {
+    echo "Gracias por su contacto. Le responderemos a la brevedad.";
+  } else {
+    echo "Hubo un error al enviar el correo electrónico. Inténtelo de nuevo más tarde.";
+  }
+} else {
+  echo "No se recibieron todos los datos del formulario.";
+}
+
+?>
