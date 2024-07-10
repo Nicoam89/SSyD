62ff465b15ebfbb76f0abff2e886066f4eb0e4d2
nicoam89 <nico.am89@gmail.com>
2024-07-10 11:27:09 -0300

    se modifica php de contacto

--- contacto.php
+++ contacto.php
@@ -1,14 +1,3 @@
-Tu código PHP es funcional y debería cumplir con el propósito de enviar un correo electrónico desde un formulario web. Aquí tienes algunas recomendaciones y mejoras para asegurar que el código sea más robusto y seguro:
-
-Validación de datos: Es importante validar y sanitizar los datos que provienen de un formulario para evitar posibles ataques como la inyección de correo electrónico.
-Manejo de errores: Asegúrate de tener un buen manejo de errores y logueo para diagnosticar posibles problemas.
-Uso de filtros de PHP: Utilizar filter_var para validar y sanitizar los datos de entrada.
-Escapar salida: Escapar las salidas para prevenir ataques XSS.
-
-Aquí te dejo una versión mejorada:
-
-php
-
 <?php
 
 if (isset($_POST['nombre']) && isset($_POST['email']) && isset($_POST['mensaje'])) {
